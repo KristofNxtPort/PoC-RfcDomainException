21ef670662ebdcff93dad4628fed72a9448c4f6d
Kristof Dieltjens <kristof.dieltjens@nxtport.com>
2020-12-18 08:38:31 +0100

    Add basic web api project

---
+++ src/PoC-RfcDomainException.ApiHost/Controllers/HelloWorldsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PoC_RfcDomainException.ApiHost.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloWorldsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
